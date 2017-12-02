2bbb9d6f8b942ec630e44d80c4445bed223c5fda
attilamandoki <attilamandoki@gmail.com>
2017-12-02 17:11:16 +0100

    update: Week-02 exercises done up until Day-3 exercise List-02

--- Week-02/Day-3/Lists/02/src/Matchmaking.java
+++ Week-02/Day-3/Lists/02/src/Matchmaking.java
@@ -1,13 +1,26 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 
-public class Matchmaking{
-  public static void main(String... args){
-    ArrayList<String> girls = new ArrayList<String>(Arrays.asList("Eve","Ashley","Bözsi","Kat","Jane"));
-    ArrayList<String> boys = new ArrayList<String>(Arrays.asList("Joe","Fred","Béla","Todd","Neef","Jeff"));
+public class Matchmaking {
+  public static void main(String... args) {
+    ArrayList<String> girls = new ArrayList<String>(Arrays.asList("Eve", "Ashley", "Bözsi", "Kat", "Jane"));
+    ArrayList<String> boys = new ArrayList<String>(Arrays.asList("Joe", "Fred", "Béla", "Todd", "Neef", "Jeff"));
     ArrayList<String> order = new ArrayList<String>();
 
+    int combinedSize = Math.max(girls.size(), boys.size());
+
+    for (int i = 0; i < combinedSize; i++) {
+      if (i < girls.size()) {
+        order.add(girls.get(i));
+        if (i < boys.size()) {
+          order.add(boys.get(i));
+        }
+      }
+    }
+
+
     // Join the two lists by matching one girl with one boy in the order list
-    // Exepected output: "Eve", "Joe", "Ashley", "Fred"...
+    // Expected output: "Eve", "Joe", "Ashley", "Fred"...
 
     System.out.println(order);
   }
