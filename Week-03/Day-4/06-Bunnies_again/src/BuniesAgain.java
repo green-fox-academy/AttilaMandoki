e2cf8f63588917622bcb49cf49badfc7cae13484
attilamandoki <attilamandoki@gmail.com>
2017-12-07 16:49:45 +0100

    update(Week-03, Day-4) Exercises done up to exercise 06-BunniesAgain

--- Week-03/Day-4/06-Bunnies_again/src/BuniesAgain.java
+++ Week-03/Day-4/06-Bunnies_again/src/BuniesAgain.java
@@ -1,6 +1,21 @@
 public class BuniesAgain {
-}
+  public static void main(String[] args) {
+    int mutantBunnyArmy = (int) (Math.random() * (100 - 1) + 1);
+    System.out.println(mutantBunnyArmy);
+    System.out.println(earCounter(mutantBunnyArmy));
 
+  }
+  public static int earCounter(int bunnyRow) {
+    if (bunnyRow == 0) {
+      return 0;
+    }
+    else if (bunnyRow % 2 == 0) {
+      return 2 + earCounter(bunnyRow - 1);
+    } else {
+        return 3 + earCounter(bunnyRow - 1);
+    }
+  }
+}
 
 // We have bunnies standing in a line, numbered 1, 2, ... The odd bunnies
 // (1, 3, ..) have the normal 2 ears. The even bunnies (2, 4, ..) we'll say
