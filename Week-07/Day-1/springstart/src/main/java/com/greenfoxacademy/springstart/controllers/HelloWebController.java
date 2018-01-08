3d932d6d24fe567b5b833a7922b01faf41cecdfc
attilamandoki <attilamandoki@gmail.com>
2018-01-08 16:53:17 +0100

    feat(Week-07, Day-1)springstart: 6/7 exercises done

--- Week-07/Day-1/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ Week-07/Day-1/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -1,15 +1,21 @@
 package com.greenfoxacademy.springstart.controllers;
 
+import com.greenfoxacademy.springstart.models.Greeting;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 @Controller
 public class HelloWebController {
+  AtomicLong idCounter = new AtomicLong();
 
-  @RequestMapping("/web/greeting")
-  public String greeting(Model model) {
-    model.addAttribute("name", " World");
+  @RequestMapping(value = "/web/greeting")
+  public String greeting(Model model, @RequestParam(value = "name", required = false) String content) {
+    model.addAttribute("name", content);
+    model.addAttribute("loadTime", idCounter.incrementAndGet());
     return "greeting";
   }
 }
