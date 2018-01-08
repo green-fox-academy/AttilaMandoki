66aec2634ba33f0cf7383da7c58a732bf0be7b78
attilamandoki <attilamandoki@gmail.com>
2018-01-08 15:52:22 +0100

    feat(Week-07, Day-1)springstart: 3/7 exercises done

--- Week-07/Day-1/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ Week-07/Day-1/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -1,16 +1,23 @@
 package com.greenfoxacademy.springstart.controllers;
 
 import com.greenfoxacademy.springstart.models.Greeting;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloRESTController {
 
 
-  @RequestMapping
-  public Greeting sayHi() {
-    Greeting greeting = new Greeting();
+  @GetMapping("/greeting")
+  public Greeting sayHi(Model model, @RequestParam(value = "content", required = false) String content) {
+    if (content == null) {
+      content = "You suck!";
+    }
+    model.addAttribute("content", content);
+    Greeting greeting = new Greeting(1, content);
     return greeting;
   }
 }
+
