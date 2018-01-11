f9ab6661dc472f90fecdb01308985a00786f4aac
attilamandoki <attilamandoki@gmail.com>
2018-01-11 11:50:00 +0100

    feat(BankOfSimba): exercise Marking kings - almost done, needs fixing

---
+++ Week-07/Day-4/UsingDependencies/src/test/java/com/greenfoxacademy/using_dependencies/UsingDependenciesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.greenfoxacademy.using_dependencies;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UsingDependenciesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
