2b67075d991b9b82fff13e31a72f6c74617a2eac
attilamandoki <attilamandoki@gmail.com>
2018-02-12 17:20:44 +0100

    feat(RestController): New API endpoint added, working

--- Week-10/practice_cognitive_apprenticeship/src/main/java/com/greenfoxacademy/practice_cognitive_apprenticeship/controllers/PetShopRestController.java
+++ Week-10/practice_cognitive_apprenticeship/src/main/java/com/greenfoxacademy/practice_cognitive_apprenticeship/controllers/PetShopRestController.java
@@ -3,6 +3,9 @@ package com.greenfoxacademy.practice_cognitive_apprenticeship.controllers;
 import com.greenfoxacademy.practice_cognitive_apprenticeship.Services.CatService;
 import com.greenfoxacademy.practice_cognitive_apprenticeship.Services.OwnerService;
 import com.greenfoxacademy.practice_cognitive_apprenticeship.models.Cat;
+import com.greenfoxacademy.practice_cognitive_apprenticeship.models.Owner;
+import com.greenfoxacademy.practice_cognitive_apprenticeship.repositories.CatRepository;
+import com.greenfoxacademy.practice_cognitive_apprenticeship.repositories.OwnerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,6 +19,12 @@ public class PetShopRestController {
 
   @Autowired
   OwnerService ownerService;
+
+  @Autowired
+  OwnerRepository ownerRepository;
+
+  @Autowired
+  CatRepository catRepository;
 
   @RequestMapping(value = "/api/petshops", method = RequestMethod.GET)
   public List<Cat> soldCats() {
@@ -31,7 +40,22 @@ public class PetShopRestController {
       return catsByBreed;
     }
   }
-  //@PostMapping("/api/newfoster/{name}")
+
+  @PostMapping("/api/newfoster/{name}")
+  public void newOwner(@PathVariable String name) {
+    Owner owner1 = new Owner();
+    owner1.setOwnerName(name);
+    ownerRepository.save(owner1);
+  }
+
+  @PostMapping("/api/newcat/{catname}/{catbreed}")
+  public void submitNewCat(@PathVariable String catname, String catbreed) {
+    Cat cat1 = new Cat();
+    cat1.setName(catname);
+    cat1.setBreed(catbreed);
+    catRepository.save(cat1);
+    //TODO: This is not working like this, needs to be fixed. 
+  }
 }
 
 
