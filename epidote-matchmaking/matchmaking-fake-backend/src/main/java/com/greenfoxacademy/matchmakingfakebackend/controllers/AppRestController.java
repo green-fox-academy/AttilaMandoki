f1abe73da071517be65ff8c707cee684762a5ab3
attilamandoki <attilamandoki@gmail.com>
2018-02-26 17:52:28 +0100

    feat(db): created db schema, changed application properties

---
+++ epidote-matchmaking/matchmaking-fake-backend/src/main/java/com/greenfoxacademy/matchmakingfakebackend/controllers/AppRestController.java
@@ -0,0 +1,9 @@
+package com.greenfoxacademy.matchmakingfakebackend.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppRestController {
+
+
+}
