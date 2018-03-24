1ddd3053d58d4d4759f94036c6f13c522e4ecaf0
attilamandoki <attilamandoki@gmail.com>
2018-03-24 19:25:52 +0100

    style guide

--- Week-02/Day-3/Lists/06/src/IsInList.java
+++ Week-02/Day-3/Lists/06/src/IsInList.java
@@ -1,19 +1,16 @@
 import java.util.*;
 
-public class IsInList{
-  public static void main(String... args){
-    ArrayList<Integer> list = new ArrayList<Integer>(Arrays.asList(2, 4, 6, 8, 10, 12, 14, 16));
+public class IsInList {
+  public static void main(String... args) {
 
-    // Check if list contains all of the following elements: 4,8,12,16
-    // Print "true" if it contains all, otherwise print "false"
-    // Can you do both the different approaches you tried in the previous one?
-
-    ArrayList<Integer> toBeChecked = new ArrayList<>(Arrays.asList(4,8,12,16));
+    ArrayList<Integer> list = new ArrayList<>(Arrays.asList(2, 4, 6, 8, 10, 12, 14, 16));
+    ArrayList<Integer> toBeChecked = new ArrayList<>(Arrays.asList(4, 8, 12, 16));
 
     if (list.containsAll(toBeChecked)) {
-      System.out.println("Hoorray");
+      System.out.println("Hoooray");
     } else {
       System.out.println("Noooooo");
     }
   }
+
 }
