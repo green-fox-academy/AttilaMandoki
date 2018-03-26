40bc47e1fc35d72b9ad940bc0fec55d06b7c13a1
attilamandoki <attilamandoki@gmail.com>
2018-03-26 16:44:14 +0200

    style guide

--- finalExamTask18/src/com/greenfoxacademy/Main.java
+++ finalExamTask18/src/com/greenfoxacademy/Main.java
@@ -6,28 +6,29 @@ import java.util.Set;
 public class Main {
 
   public static void main(String[] args) {
-    String firstString = "Create a function that takes two strings and returns an array of";
+    String firstString = "Create a function that takes tWo strings and returns an array of";
     String secondString = "characters that consists all the common letters of the two arrays";
 
-    String commonLetters = findCommonChars(firstString, secondString);
-    char[] charArrayOfLetters = commonLetters.toCharArray();
-    System.out.println(charArrayOfLetters);
-
+    System.out.println(converter(findCommonChars(firstString, secondString)));
   }
 
   private static String findCommonChars(String firstInput, String secondInput) {
     StringBuilder resultBuilder = new StringBuilder();
-    Set<Character> charsMap = new HashSet<>();
+    Set<Character> characterMap = new HashSet<>();
     for (int i = 0; i < firstInput.length(); i++) {
-      char charAt = firstInput.charAt(i);
-      if (secondInput.indexOf(charAt) != -1) {
-        charsMap.add(charAt);
+      char charAt = firstInput.toLowerCase().charAt(i);
+      if (secondInput.toLowerCase().indexOf(charAt) != -1) {
+        characterMap.add(charAt);
       }
     }
-    for (Character aCharsMap : charsMap) {
+    for (Character aCharsMap : characterMap) {
       resultBuilder.append(aCharsMap.charValue());
     }
     return resultBuilder.toString();
+  }
+
+  private static char[] converter(String inputString) {
+    return inputString.toCharArray();
   }
 
 }
