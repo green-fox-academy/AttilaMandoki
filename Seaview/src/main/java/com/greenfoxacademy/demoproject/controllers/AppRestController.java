ca4ea05fc2cb4df78c04f930e7c15feefe7d466e
attilamandoki <attilamandoki@gmail.com>
2018-05-10 16:48:13 +0200

    feat(app)extending model and some small changes

--- Seaview/src/main/java/com/greenfoxacademy/demoproject/controllers/AppRestController.java
+++ Seaview/src/main/java/com/greenfoxacademy/demoproject/controllers/AppRestController.java
@@ -1,5 +1,8 @@
 package com.greenfoxacademy.demoproject.controllers;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AppRestController {
 
 
--- Seaview/src/main/java/com/greenfoxacademy/demoproject/models/ForecastData.java
+++ Seaview/src/main/java/com/greenfoxacademy/demoproject/models/ForecastData.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.sql.Time;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -12,13 +14,26 @@ import lombok.Setter;
 public class ForecastData {
 
   private String source;
+  private Time time;
   private double swellHeight;
   private double swellDirection;
   private double swellPeriod;
   private double waveHeight;
   private double waveDirection;
   private double wavePeriod;
+  private double waterTemperature;
   private double windSpeed;
   private double windDirection;
+  private double windGust;
+  //windGust is in m/s (might have to change data type)
   private double airTemperature;
+  private double airPressure;
+  private double humidity;
+  private double cloudCover;
+  private double precipitation;
+  private double visiblity;
+  private double dailyQuota;
+  private double latitude;
+  private double longitude;
+  private double requestCount;
 }
