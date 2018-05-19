7eea6d3317e478a56ace7f3951c9464a08961e06
attilamandoki <attilamandoki@gmail.com>
2018-05-19 17:34:10 +0200

    feat(app): few changes

--- Seaview/src/main/java/com/greenfoxacademy/demoproject/services/StormGlassAPIService.java
+++ Seaview/src/main/java/com/greenfoxacademy/demoproject/services/StormGlassAPIService.java
@@ -7,11 +7,13 @@ import retrofit2.http.GET;
 import retrofit2.http.Headers;
 import retrofit2.http.Path;
 
+import java.util.List;
+
 @Service
 public interface StormGlassAPIService {
 
   //data is to be replaced
   @Headers({"Content-Type: application/json", "Accept: application/json"})
   @GET("data/{data}")
-  Call<ForecastData> getForecastData(@Path("data") String data);
+  Call<List<ForecastData>> getForecastData(@Path("") String data);
 }
