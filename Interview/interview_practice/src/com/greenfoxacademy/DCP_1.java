2d871a9a3407649be474d9abd8293f18e629698e
attilamandoki <attilamandoki@gmail.com>
2018-06-05 15:16:04 +0200

    setting up new tasks

--- Interview/interview_practice/src/com/greenfoxacademy/DCP_1.java
+++ Interview/interview_practice/src/com/greenfoxacademy/DCP_1.java
@@ -2,9 +2,9 @@ package com.greenfoxacademy;
 
 import java.util.Arrays;
 
-public class DCP_1 {
+class DCP_1 {
 
-  public boolean hasArrayTwoCandidates(int A[], int arr_size, int sum) {
+  boolean hasArrayTwoCandidates(int A[], int arr_size, int sum) {
     int l, r;
     Arrays.sort(A);
 
@@ -22,5 +22,4 @@ public class DCP_1 {
   }
 }
 /*Given a list of numbers, return whether any two sums to k.
-For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
-Bonus: Can you do this in one pass?*/
+For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.*/
--- Interview/interview_practice/src/com/greenfoxacademy/DCP_2.java
+++ Interview/interview_practice/src/com/greenfoxacademy/DCP_2.java
@@ -1,4 +1,22 @@
 package com.greenfoxacademy;
 
+import java.util.Arrays;
+
 public class DCP_2 {
+
+  int[] productOfAllButI(int[] ints) {
+    int y = ints.length - 1;;
+    Arrays.sort(ints);
+
+    for (int i = 0; i < y; i++) {
+    }
+    return null;
+  }
 }
+/*Given an array of integers, return a new array such that each element
+at index i of the new array is the product of all the numbers in the original
+array except the one at i.
+For example, if our input was [1, 2, 3, 4, 5], the expected output would be
+[120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would be [2, 3, 6].*/
+
+//solution: https://www.geeksforgeeks.org/a-product-array-puzzle/
