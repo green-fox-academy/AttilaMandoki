9d53bafa8050e44458896e16e5f38179d8c79232
attilamandoki <attilamandoki@gmail.com>
2018-06-09 10:55:12 +0200

    rebasing project

---
+++ private_projects/hskpr/src/main/java/com/accorhotelsbudapesthackathon/hskpr/HskprApplication.java
@@ -0,0 +1,12 @@
+package com.accorhotelsbudapesthackathon.hskpr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HskprApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HskprApplication.class, args);
+  }
+}
